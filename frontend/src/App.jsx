41331db0b16479147b2e5e35c05af8715f320b4f
suruthika <suruthikachidambararaj@gmail.com>
2026-06-10 21:50:21 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import * as icons from 'lucide-react';
 
 import './App.css';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://erp-portal-2-4gqh.onrender.com';
 
 // --- Login Component ---
 function Login({ onLogin }) {
